26005e41a1eb53d345124408278bb7bb7ce98c48
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2022-07-27 10:41:43 +0530

    Add Module 2

---
+++ Module-2/src/gdse/ClientApp.java
@@ -0,0 +1,16 @@
+package gdse;
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+/**
+ * @author : Nimesh Piyumantha
+ * @since : 0.1.0
+ **/
+public class ClientApp {
+    public static void main(String[] args) throws IOException {
+
+    }
+}
---
+++ Module-2/src/gdse/SeverApp.java
@@ -0,0 +1,15 @@
+package gdse;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ * @author : Nimesh Piyumantha
+ * @since : 0.1.0
+ **/
+public class SeverApp {
+    public static void main(String[] args) throws IOException {
+
+    }
+}
