88a4e29df88d54240378089cec1a34a534fa63dc
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2022-07-27 10:43:30 +0530

    Add Sever App Code

--- Module-2/src/gdse/SeverApp.java
+++ Module-2/src/gdse/SeverApp.java
@@ -10,6 +10,32 @@ import java.net.Socket;
  **/
 public class SeverApp {
     public static void main(String[] args) throws IOException {
+        final int PORT = 8000;
+        try {
+            ServerSocket serverSocket = new ServerSocket(PORT);
+            System.out.println("Server is running in port: " + PORT);
 
+            Socket localSocket = serverSocket.accept();
+            System.out.println("Client accepted..!");
+
+            DataOutputStream dataOutputStream = new DataOutputStream(localSocket.getOutputStream());
+            DataInputStream dataInputStream = new DataInputStream(localSocket.getInputStream());
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+            String message = "", reply = "";
+
+            while (!message.equals("finish")) {
+                message = dataInputStream.readUTF();
+                System.out.println(message);
+                reply = bufferedReader.readLine();
+                dataOutputStream.writeUTF(reply);
+                dataOutputStream.flush();
+            }
+            dataInputStream.close();
+            dataOutputStream.close();
+            bufferedReader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
