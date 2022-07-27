965c29e4c150d85fc6088a38aae81014c32fc675
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2022-07-27 11:00:11 +0530

    Add Client App Code

--- Module-2/src/gdse/ClientApp.java
+++ Module-2/src/gdse/ClientApp.java
@@ -1,8 +1,8 @@
 package gdse;
 
 
-import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.*;
+import java.net.ServerSocket;
 import java.net.Socket;
 
 /**
@@ -11,6 +11,28 @@ import java.net.Socket;
  **/
 public class ClientApp {
     public static void main(String[] args) throws IOException {
+        final int PORT = 8000;
+        try {
+            Socket socket = new Socket("localhost", PORT);
 
+            DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream());
+            DataInputStream dataInputStream = new DataInputStream(socket.getInputStream());
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+            String message = "", reply = "";
+            while (!message.equals("finish")) {
+                reply = bufferedReader.readLine();
+                dataOutputStream.writeUTF(reply);
+
+                message = dataInputStream.readUTF();
+                System.out.println(message);
+//                dataOutputStream.flush();
+            }
+            dataInputStream.close();
+            dataOutputStream.close();
+            bufferedReader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
